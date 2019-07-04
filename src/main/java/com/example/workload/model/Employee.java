a5b2ffadfe376277c07a6b539090930ef00c3657
KasiaKoper <katarzynakoper2@gmail.com>
2019-07-04 20:03:17 +0200

    employee created

---
+++ src/main/java/com/example/workload/model/Employee.java
@@ -0,0 +1,57 @@
+package com.example.workload.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    //private task_id
+    private String name;
+    private String surname;
+    private String role;
+    private int capacity;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/workload/model/Task.java
@@ -0,0 +1,51 @@
+package com.example.workload.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    private String description;
+    private double time;
+
+    @ManyToOne
+    private Employee employee;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+
+}
